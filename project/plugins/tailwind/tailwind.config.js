69699fb7acb80939ddfc6fc21894dc5cfcd7a9af
Abdullox Abduhamidov <abdulloxfront@gmail.com>
2023-08-30 17:11:42 +0500

    bismillah

---
+++ project/plugins/tailwind/tailwind.config.js
@@ -0,0 +1,35 @@
+tailwind.config = {  
+theme: {
+  letterSpacing: {
+    currentWide: ".0.005em;",
+  },
+  
+  screens:{
+    xs: '480px',
+    sm:'640px',
+    md:'768px',
+    lg:'1024px',
+    xl:'1280px',
+    xll:'1440px',
+  },
+  container: {
+    padding: {
+      DEFAULT: '0.5rem',
+      xs:'10px',
+      sm: '0.9rem',
+      lg: '1rem',
+      xl: '1.2rem'
+    },
+    center: 'true',
+  },
+  extend: {
+    fontFamily: {
+      boldText: ['Ubuntu-bold'],
+    },
+    fontFamily: {
+      regularText: ['Ubuntu-regular'],
+    },
+    
+  },
+},
+};
